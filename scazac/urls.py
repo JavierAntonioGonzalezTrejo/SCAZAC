ba95d18294e57f6157fab1d1c907215b5b51c096
Javier Antonio Gonzalez Trejo <javier.antonio.gonzalez.trejo@gmail.com>
2017-09-29 15:02:12 -0500

    Modified the account bar to point to diferents part of the investigacion dashboard

--- scazac/urls.py
+++ scazac/urls.py
@@ -1,5 +1,6 @@
 # Report of Modifications
-# Modification 20170723: Givesd the root URL to the calidadAire app to handle. 
+# Modification 20170723: Givesd the root URL to the calidadAire app to handle.
+# 20170924: URL from requirment F3 handle by investigacion
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
@@ -9,10 +10,11 @@ import calidadAire.views
 
 
 urlpatterns = [
-    url(r"^$", calidadAire.views.index, name='home'),
-    url(r"^", include("calidadAire.urls")),                # TemplateView.as_view(template_name="homepage.html"), name="home")
+    url(r"^$", calidadAire.views.index, name='home'),#              
+    url(r"^", include("calidadAire.urls")),                # 
     url(r"^admin/", include(admin.site.urls)),
-    url(r"^account/", include("account.urls")),
+    url(r"^usuario/", include("account.urls")),
+    url(r"^investigacion/", include("investigacion.urls")), # Added 20170924
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
